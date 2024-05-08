d773531e39e017f5689e67208823568f34a18d01
Rafail <Raf_0707@mail.ru>
2024-05-08 17:42:10 +0300

    no bookmark

--- app/src/main/java/raf/tabiin/qurantajweed/MainActivity.java
+++ app/src/main/java/raf/tabiin/qurantajweed/MainActivity.java
@@ -1,5 +1,6 @@
 package raf.tabiin.qurantajweed;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -32,8 +33,11 @@ import raf.tabiin.qurantajweed.utils.BookmarksPref;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 viewPager;
-    private BookmarkAdapter bookmarkAdapter;
+    //private BookmarkAdapter bookmarkAdapter;
     private int currentPosition = 0;
+
+    private static final String PREFS_NAME = "LastPagePrefs";
+    private static final String LAST_PAGE_KEY = "last_page_key";
 
     private BookmarksPref bookmarksPref;
     ActivityMainBinding b;
@@ -51,20 +55,29 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(b.toolbar);
 
         Objects.requireNonNull(getSupportActionBar()).setTitle("");
-        b.drawerQuranLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, Gravity.LEFT);
+        //b.drawerQuranLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, Gravity.LEFT);
 
         bookmarksPref = new BookmarksPref(this);
 
         viewPager = b.viewPager;
-        bookmarkAdapter = new BookmarkAdapter(this, viewPager);
+        //bookmarkAdapter = new BookmarkAdapter(this, viewPager);
         viewPager.setLayoutDirection(ViewPager2.LAYOUT_DIRECTION_RTL);
 
         // Use ImageAdapter for ViewPager2
         ImageAdapter imageAdapter = new ImageAdapter(this, 604, viewPager);
         viewPager.setAdapter(imageAdapter);
 
-        Menu menu = b.toolbar.getMenu();
-        MenuItem addBookmarkItem = menu.findItem(R.id.action_add_bookmark);
+        /*Menu menu = b.toolbar.getMenu();
+        MenuItem addBookmarkItem = menu.findItem(R.id.action_add_bookmark);*/
+        int lastPage = loadLastPage();
+        viewPager.setCurrentItem(lastPage, false);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                // Сохраните текущий номер страницы при изменении страницы
+                saveLastPage(position);
+            }
+        });
 
         imageAdapter.setOnPageChangedListener(new OnPageChangedListener() {
             @Override
@@ -80,17 +93,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        RecyclerView bookMarksRecycle = b.bookmarksQuranRecycle;
+        /*RecyclerView bookMarksRecycle = b.bookmarksQuranRecycle;
         bookMarksRecycle.setAdapter(bookmarkAdapter);
-        bookMarksRecycle.setHasFixedSize(false);
+        bookMarksRecycle.setHasFixedSize(false);*/
 
-        bookmarkAdapter.loadBookmarks();
+        //bookmarkAdapter.loadBookmarks();
 
         // Set current position from bookmarks if available
-        List<Bookmark> bookmarks = bookmarkAdapter.getBookmarks();
+        /*List<Bookmark> bookmarks = bookmarkAdapter.getBookmarks();
         if (bookmarks != null && !bookmarks.isEmpty()) {
             viewPager.post(() -> viewPager.setCurrentItem(bookmarks.get(0).getPosition(), false));
-        }
+        }*/
 
         initContent();
         initMap();
@@ -98,9 +111,9 @@ public class MainActivity extends AppCompatActivity {
         DrawerQuranContentAdapter drawerQuranContentAdapter = new DrawerQuranContentAdapter(this, suresName, viewPager);
         RecyclerView quranContent = b.quranDrawerContent;
         quranContent.setAdapter(drawerQuranContentAdapter);
-        quranContent.setHasFixedSize(false);
+        quranContent.setHasFixedSize(true);
 
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+        /*viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
                 try {
@@ -113,126 +126,126 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
-        });
+        });*/
 
     }
 
     private void initContent() {
         sures = new String[] {
-                "Сура 1. Аль-Фатиха — Открывающая",
-                "Сура 2. Аль-Бакара — Корова",
-                "Сура 3. Али Имран — Семейство Имрана",
-                "Сура 4. Ан-Ниса — Женщины",
-                "Сура 5. Аль-Маида — Трапеза",
-                "Сура 6. Аль-Ан'ам — Скот",
-                "Сура 7. Аль-А'раф — Преграды",
-                "Сура 8. Аль-Анфаль — Трофеи",
-                "Сура 9. Ат-Тауба — Покаяние",
-                "Сура 10. Юнус — Юнус (Иона)",
-                "Сура 11. Худ — Худ",
-                "Сура 12. Йусуф — Йусуф (Иосиф)",
-                "Сура 13. Ар-Ра'д — Гром",
-                "Сура 14. Ибрахим — Ибрахим (Авраам)",
-                "Сура 15. Аль-Хиджр — Каменное плато",
-                "Сура 16. Ан-Нахль — Пчелы",
-                "Сура 17. Аль-Исра — Ночной перенос",
-                "Сура 18. Аль-Кахф — Пещера",
-                "Сура 19. Марьям — Марьям (Мария)",
-                "Сура 20. Та-Ха — Та-Ха",
-                "Сура 21. Аль-Анбия — Пророки",
-                "Сура 22. Аль-Хадж — Паломничество",
-                "Сура 23. Аль-Му'минун — Верующие",
-                "Сура 24. Ан-Нур — Свет",
-                "Сура 25. Аль-Фуркан — Различение",
-                "Сура 26. Аш-Шуара — Поэты",
-                "Сура 27. Ан-Намль — Муравьи",
-                "Сура 28. Аль-Касас — Рассказы",
-                "Сура 29. Аль-Анкабут — Паук",
-                "Сура 30. Ар-Рум — Римляне",
-                "Сура 31. Лукман — Лукман",
-                "Сура 32. Ас-Саджда — Поклон",
-                "Сура 33. Аль-Ахзаб — Союзники",
-                "Сура 34. Саба — Сабейцы",
-                "Сура 35. Фатыр — Творец",
-                "Сура 36. Йа Син — Йа Син",
-                "Сура 37. Ас-Саффат — Выстраивающиеся в ряды",
-                "Сура 38. Сад — Буква Сад",
-                "Сура 39. Аз-Зумар — Толпы",
-                "Сура 40. Гафир — Прощающий",
-                "Сура 41. Фуссылят — Разъяснены",
-                "Сура 42. Аш-Шура — Совет",
-                "Сура 43. Аз-Зухруф — Украшения",
-                "Сура 44. Ад-Духан — Дым",
-                "Сура 45. Аль-Джасия — Коленопреклоненные",
-                "Сура 46. Аль-Ахкаф — Дюны",
-                "Сура 47. Мухаммад — Мухаммад",
-                "Сура 48. Аль-Фатх — Победа",
-                "Сура 49. Аль-Худжурат — Комнаты",
-                "Сура 50. Каф — Буква Каф",
-                "Сура 51. Аль-Дариат — Рассеивающие",
-                "Сура 52. Ат-Тур — Гора",
-                "Сура 53. Ан-Наджм — Звезда",
-                "Сура 54. Аль-Камар — Луна",
-                "Сура 55. Ар-Рахман — Милостивый",
-                "Сура 56. Аль-Вакы'а — Неотвратимое событие",
-                "Сура 57. Аль-Хадид — Железо",
-                "Сура 58. Аль-Муджадала — Препирающаяся",
-                "Сура 59. Аль-Хашр — Сбор",
-                "Сура 60. Аль-Мумтахина — Испытуемая",
-                "Сура 61. Ас-Сафф — Ряд",
-                "Сура 62. Аль-Джуму'а — Пятница",
-                "Сура 63. Аль-Мунафиюн — Лицемеры",
-                "Сура 64. Ат-Тагабун — Раскрытие самообмана",
-                "Сура 65. Ат-Талак — Развод",
-                "Сура 66. Ат-Тахрим — Запрещение",
-                "Сура 67. Аль-Мульк — Власть",
-                "Сура 68. Аль-Калам — Письменная трость",
-                "Сура 69. Аль-Хакка — Неизбежное",
-                "Сура 70. Аль-Ма'аридж — Ступени",
-                "Сура 71. Нух — Нух (Ной)",
-                "Сура 72. Аль-Джинн — Джинны",
-                "Сура 73. Аль-Муззаммиль — Закутавшийся",
-                "Сура 74. Аль-Муддассир — Завернувшийся",
-                "Сура 75. Аль-Кыяма — Воскресение",
-                "Сура 76. Аль-Инсан — Человек",
-                "Сура 77. Аль-Мурсалят — Посланные",
-                "Сура 78. Ан-Наба — Весть",
-                "Сура 79. Ан-Назиат — Вырывающие",
-                "Сура 80. Абаса — Нахмурившийся",
-                "Сура 81. Ат-Таквир — Скручивание",
-                "Сура 82. Аль-Инфитар — Раскалывание",
-                "Сура 83. Аль-Мутаффифин — Обвешивающие",
-                "Сура 84. Аль-Иншикак — Разверзнется",
-                "Сура 85. Аль-Бурудж — Созвездия",
-                "Сура 86. Ат-Тарик — Ночной путник",
-                "Сура 87. Аль-А'ля — Высочайший",
-                "Сура 88. Аль-Гашия — Покрывающее",
-                "Сура 89. Аль-Фаджр — Заря",
-                "Сура 90. Аль-Баляд — Город",
-                "Сура 91. Аш-Шамс — Солнце",
-                "Сура 92. Аль-Ляйл — Ночь",
-                "Сура 93. Ад-Духа — Утро",
-                "Сура 94. Аш-Шарх — Раскрытие",
-                "Сура 95. Ат-Тин — Инжир",
-                "Сура 96. Аль-'Аляк — Сгусток крови",
-                "Сура 97. Аль-Кадр — Ночь предопределения",
-                "Сура 98. Аль-Баййина — Ясное знамение",
-                "Сура 99. Аз-Зальзаля — Землетрясение",
-                "Сура 100. Аль-'Адият — Скачущие",
-                "Сура 101. Аль-Кари'а — Великое бедствие",
-                "Сура 102. Ат-Такасур — Страсть к приумножению",
-                "Сура 103. Аль-'Аср — Предвечернее время",
-                "Сура 104. Аль-Хумаза — Хулитель",
-                "Сура 105. Аль-Филь — Слон",
-                "Сура 106. Курайш — Курайшиты",
-                "Сура 107. Аль-Ма'ун — Мелочь",
-                "Сура 108. Аль-Кяусар — Изобилие",
-                "Сура 109. Аль-Кяфирун — Неверующие",
-                "Сура 110. Ан-Наср — Победа",
-                "Сура 111. Аль-Ляхаб — Пальмовые волокна",
-                "Сура 112. Аль-Ихляс — Искренность",
-                "Сура 113. Аль-Фаляк — Рассвет",
-                "Сура 114. Ан-Нас — Люди"
+                "Сура 1. Аль-Фатиха\nОткрывающая",
+                "Сура 2. Аль-Бакара\nКорова",
+                "Сура 3. Али Имран\nСемейство Имрана",
+                "Сура 4. Ан-Ниса\nЖенщины",
+                "Сура 5. Аль-Маида\nТрапеза",
+                "Сура 6. Аль-Ан'ам\nСкот",
+                "Сура 7. Аль-А'раф\nПреграды",
+                "Сура 8. Аль-Анфаль\nТрофеи",
+                "Сура 9. Ат-Тауба\nПокаяние",
+                "Сура 10. Юнус\nЮнус (Иона)",
+                "Сура 11. Худ\nХуд",
+                "Сура 12. Йусуф\nЙусуф (Иосиф)",
+                "Сура 13. Ар-Ра'д\nГром",
+                "Сура 14. Ибрахим\nИбрахим (Авраам)",
+                "Сура 15. Аль-Хиджр\nКаменное плато",
+                "Сура 16. Ан-Нахль\nПчелы",
+                "Сура 17. Аль-Исра\nНочной перенос",
+                "Сура 18. Аль-Кахф\nПещера",
+                "Сура 19. Марьям\nМарьям (Мария)",
+                "Сура 20. Та-Ха\nТа-Ха",
+                "Сура 21. Аль-Анбия\nПророки",
+                "Сура 22. Аль-Хадж\nПаломничество",
+                "Сура 23. Аль-Му'минун\nВерующие",
+                "Сура 24. Ан-Нур\nСвет",
+                "Сура 25. Аль-Фуркан\nРазличение",
+                "Сура 26. Аш-Шуара\nПоэты",
+                "Сура 27. Ан-Намль\nМуравьи",
+                "Сура 28. Аль-Касас\nРассказы",
+                "Сура 29. Аль-Анкабут\nПаук",
+                "Сура 30. Ар-Рум\nРимляне",
+                "Сура 31. Лукман\nЛукман",
+                "Сура 32. Ас-Саджда\nПоклон",
+                "Сура 33. Аль-Ахзаб\nСоюзники",
+                "Сура 34. Саба\nСабейцы",
+                "Сура 35. Фатыр\nТворец",
+                "Сура 36. Йа Син\nЙа Син",
+                "Сура 37. Ас-Саффат\nВыстраивающиеся в ряды",
+                "Сура 38. Сад\nБуква Сад",
+                "Сура 39. Аз-Зумар\nТолпы",
+                "Сура 40. Гафир\nПрощающий",
+                "Сура 41. Фуссылят\nРазъяснены",
+                "Сура 42. Аш-Шура\nСовет",
+                "Сура 43. Аз-Зухруф\nУкрашения",
+                "Сура 44. Ад-Духан\nДым",
+                "Сура 45. Аль-Джасия\nКоленопреклоненные",
+                "Сура 46. Аль-Ахкаф\nДюны",
+                "Сура 47. Мухаммад\nМухаммад",
+                "Сура 48. Аль-Фатх\nПобеда",
+                "Сура 49. Аль-Худжурат\nКомнаты",
+                "Сура 50. Каф\nБуква Каф",
+                "Сура 51. Аль-Дариат\nРассеивающие",
+                "Сура 52. Ат-Тур\nГора",
+                "Сура 53. Ан-Наджм\nЗвезда",
+                "Сура 54. Аль-Камар\nЛуна",
+                "Сура 55. Ар-Рахман\nМилостивый",
+                "Сура 56. Аль-Вакы'а\nНеотвратимое событие",
+                "Сура 57. Аль-Хадид\nЖелезо",
+                "Сура 58. Аль-Муджадала\nПрепирающаяся",
+                "Сура 59. Аль-Хашр\nСбор",
+                "Сура 60. Аль-Мумтахина\nИспытуемая",
+                "Сура 61. Ас-Сафф\nРяд",
+                "Сура 62. Аль-Джуму'а\nПятница",
+                "Сура 63. Аль-Мунафиюн\nЛицемеры",
+                "Сура 64. Ат-Тагабун\nРаскрытие самообмана",
+                "Сура 65. Ат-Талак\nРазвод",
+                "Сура 66. Ат-Тахрим\nЗапрещение",
+                "Сура 67. Аль-Мульк\nВласть",
+                "Сура 68. Аль-Калам\nПисьменная трость",
+                "Сура 69. Аль-Хакка\nНеизбежное",
+                "Сура 70. Аль-Ма'аридж\nСтупени",
+                "Сура 71. Нух\nНух (Ной)",
+                "Сура 72. Аль-Джинн\nДжинны",
+                "Сура 73. Аль-Муззаммиль\nЗакутавшийся",
+                "Сура 74. Аль-Муддассир\nЗавернувшийся",
+                "Сура 75. Аль-Кыяма\nВоскресение",
+                "Сура 76. Аль-Инсан\nЧеловек",
+                "Сура 77. Аль-Мурсалят\nПосланные",
+                "Сура 78. Ан-Наба\nВесть",
+                "Сура 79. Ан-Назиат\nВырывающие",
+                "Сура 80. Абаса\nНахмурившийся",
+                "Сура 81. Ат-Таквир\nСкручивание",
+                "Сура 82. Аль-Инфитар\nРаскалывание",
+                "Сура 83. Аль-Мутаффифин\nОбвешивающие",
+                "Сура 84. Аль-Иншикак\nРазверзнется",
+                "Сура 85. Аль-Бурудж\nСозвездия",
+                "Сура 86. Ат-Тарик\nНочной путник",
+                "Сура 87. Аль-А'ля\nВысочайший",
+                "Сура 88. Аль-Гашия\nПокрывающее",
+                "Сура 89. Аль-Фаджр\nЗаря",
+                "Сура 90. Аль-Баляд\nГород",
+                "Сура 91. Аш-Шамс\nСолнце",
+                "Сура 92. Аль-Ляйл\nНочь",
+                "Сура 93. Ад-Духа\nУтро",
+                "Сура 94. Аш-Шарх\nРаскрытие",
+                "Сура 95. Ат-Тин\nИнжир",
+                "Сура 96. Аль-'Аляк\nСгусток крови",
+                "Сура 97. Аль-Кадр\nНочь предопределения",
+                "Сура 98. Аль-Баййина\nЯсное знамение",
+                "Сура 99. Аз-Зальзаля\nЗемлетрясение",
+                "Сура 100. Аль-'Адият\nСкачущие",
+                "Сура 101. Аль-Кари'а\nВеликое бедствие",
+                "Сура 102. Ат-Такасур\nСтрасть к приумножению",
+                "Сура 103. Аль-'Аср\nПредвечернее время",
+                "Сура 104. Аль-Хумаза\nХулитель",
+                "Сура 105. Аль-Филь\nСлон",
+                "Сура 106. Курайш\nКурайшиты",
+                "Сура 107. Аль-Ма'ун\nМелочь",
+                "Сура 108. Аль-Кяусар\nИзобилие",
+                "Сура 109. Аль-Кяфирун\nНеверующие",
+                "Сура 110. Ан-Наср\nПобеда",
+                "Сура 111. Аль-Ляхаб\nПальмовые волокна",
+                "Сура 112. Аль-Ихляс\nИскренность",
+                "Сура 113. Аль-Фаляк\nРассвет",
+                "Сура 114. Ан-Нас\nЛюди"
         };
     }
 
@@ -243,7 +256,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void updateBookmarkIcon(int page, MenuItem menuItem) {
+    /*public void updateBookmarkIcon(int page, MenuItem menuItem) {
         try {
             // Проверка наличия закладки на текущей странице
             boolean isBookmarked = bookmarkAdapter.isBookmarked(page);
@@ -259,9 +272,9 @@ public class MainActivity extends AppCompatActivity {
             // Вывод исключения в лог для отладки
             e.printStackTrace();
         }
-    }
+    }*/
 
-    public void toggleBookmark(int page) {
+    /*public void toggleBookmark(int page) {
         if (bookmarkAdapter.isBookmarked(page)) {
             bookmarkAdapter.removeBookmark(page);
         } else {
@@ -271,6 +284,20 @@ public class MainActivity extends AppCompatActivity {
         Menu menu = b.toolbar.getMenu();
         MenuItem addBookmarkItem = menu.findItem(R.id.action_add_bookmark);
         updateBookmarkIcon(page, addBookmarkItem);
+    }*/
+
+    // Метод для сохранения последней страницы в SharedPreferences
+    private void saveLastPage(int lastPage) {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(LAST_PAGE_KEY, lastPage);
+        editor.apply();
+    }
+
+    // Метод для загрузки последней страницы из SharedPreferences
+    private int loadLastPage() {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return prefs.getInt(LAST_PAGE_KEY, 0); // По умолчанию начнем с 0 страницы
     }
 
     @Override
@@ -285,10 +312,10 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch (id) {
             case R.id.action_add_bookmark:
-                int currentPosition = bookmarkAdapter.getCurrentPosition();
-                boolean isBookmarked = bookmarkAdapter.isBookmarked(currentPosition);
+                //int currentPosition = bookmarkAdapter.getCurrentPosition();
+                //boolean isBookmarked = bookmarkAdapter.isBookmarked(currentPosition);
 
-                if (isBookmarked) {
+                /*if (isBookmarked) {
                     // Если страница уже в закладках, удалите закладку
                     bookmarkAdapter.removeBookmark(currentPosition);
                     // Установите иконку bookmark_empty
@@ -299,7 +326,7 @@ public class MainActivity extends AppCompatActivity {
                     bookmarkAdapter.addBookmark(bookmark);
                     // Установите иконку bookmark_full
                     item.setIcon(R.drawable.bookmark_full);
-                }
+                }*/
                 return true;
             case R.id.action_view_bookmarks:
                 b.drawerQuranLayout.closeDrawer(Gravity.LEFT);
@@ -317,6 +344,11 @@ public class MainActivity extends AppCompatActivity {
     public interface OnPageChangedListener {
         void onPageChanged(int position);
         int getCurrentPosition();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
 }
